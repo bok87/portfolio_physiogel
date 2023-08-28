88541c6bd7ae6187674e919144cf9011b8e20833
boksun <hpqnls17@naver.com>
2023-08-28 16:47:01 +0900

    0828

--- script/common.js
+++ script/common.js
@@ -1,7 +1,7 @@
 // const nav = document.querySelectorAll('#nav_menu')
-// const open_nav = document.querySelector('#open_nav')
+const open_nav = document.querySelector('#open_nav')
 // const nav_close = document.querySelector('#nav_close')
-// console.log = (/*nav , open_nav, nav_close */)
+console.log = (/*nav , open_nav, nav_close */)
 // swiper-slide
 // const adver_bnr = new Swiper('#adver_bnr',{
 //     direction: 'horizontal',
@@ -23,18 +23,20 @@
 //     },
 //     });
 
+var swiper = new Swiper("#main_bnr", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     scrollbar: {
         el: ".swiper-scrollbar",
     },
-    // navigation: {
-    //     nextEl: ".swiper-button-next",
-    //     prevEl: ".swiper-button-prev",
+    // pagination: {
+    //     el: ".swiper-scrollbar",
+    //     clickable: true,
     // },
-    pagination: {
-        el: ".swiper-scrollbar",
-        clickable: true,
-    },
 });
